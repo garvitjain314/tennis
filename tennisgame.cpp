7156212a06278205c2727bb12ba2ecd3302ded07
radhika khetan <mnnit.radhika@gmail.com>
2015-07-27 13:35:38 +0530

    Adding displayCurrentScore function

--- tennisgame.cpp
+++ tennisgame.cpp
@@ -23,17 +23,14 @@ class TennisGame {
 			pair<int, int> currentScore = scoreBoard[scoreBoard.size() - 1];
 			pair<int, int> newScore = scorer == 'D' ? pair<int, int> (currentScore.first + 1, currentScore.second) : pair<int, int> (currentScore.first, currentScore.second + 1);
 			if(isValid(newScore)){
-				cout << newScore.first << " ";
-				cout << newScore.second << endl ;
 				scoreBoard.push_back(newScore);
 				return;
 			}
 			cout << "The entered string does not represent a game" << endl;
 			return;
 		}
-		void displayWholeScoreboard ( int index ) {
-        for ( int i = 0 ; i <= index ; i++ ) {
-
+    void displayFullScoreboard ( ) {
+        for ( int i = 0 ; i < scoreBoard.size() ; i++ ) {
             if (scoreBoard[i].first >= 3 &&  scoreBoard[i].second >=3) {
                 if (scoreBoard[i].first==scoreBoard[i].second)
                     cout << "deuce" << endl;
@@ -43,51 +40,47 @@ class TennisGame {
                     cout << "D advantage" << endl;
                 else if ( scoreBoard[i].first == (scoreBoard[i].second + 2)) {
                     cout << "D wins" << endl ;
-                    break;
+                    return;
                 }
-
                 else if ( (scoreBoard[i].first + 2) == scoreBoard[i].second ) {
                     cout << "F wins" << endl;
-                    break;
+                    return;
                 }
-
             }
             else if( scoreBoard[i].first > 3 || scoreBoard[i].second > 3 ) {
                 if( scoreBoard[i].first - scoreBoard[i].second >= 2 ) {
                     cout << "D wins" << endl;
                     break;
                 }
-
                 else if ( scoreBoard[i].second - scoreBoard[i].first >= 2 ) {
                     cout << "F wins" << endl;
                 }
-
             }
             else
-                    cout << scoreEquivalence[scoreBoard[i].first] << " " << scoreEquivalence[scoreBoard[i].second] << endl ;
+                cout << "D-" <<scoreEquivalence[scoreBoard[i].first] << " " << "F-" << scoreEquivalence[scoreBoard[i].second] << endl ;
         }
-        }
+    }
         void displayCurrentScore ( int i ) {
-            if( scoreBoard[i].first > 3 || scoreBoard[i].second > 3 ) {
-                if( scoreBoard[i].first - scoreBoard[i].second >= 2 )
-                    cout << "D wins" ;
-                else if ( scoreBoard[i].second - scoreBoard[i].first >= 2 )
-                    cout << "F wins" ;
-            }
-            else if (scoreBoard[i].first >= 4 &&  scoreBoard[i].second >=4) {
+            if (scoreBoard[i].first >= 3 &&  scoreBoard[i].second >=3) {
                 if (scoreBoard[i].first==scoreBoard[i].second)
-                    cout << "deuce" ;
-                else if  (scoreBoard[i].first  + 1==scoreBoard[i].second )
-                    cout << "F advantage" ;
-                else if (scoreBoard[i].first == scoreBoard[i].second + 1 )
-                    cout << "D advantage" ;
-                else if ( scoreBoard[i].first == scoreBoard[i].second + 2)
-                    cout << "D wins" ;
-                else if ( scoreBoard[i].first + 2 == scoreBoard[i].second )
-                    cout << "F wins" ;
+                    cout << "deuce" << endl;
+                else if  ( (scoreBoard[i].first + 1) == scoreBoard[i].second )
+                    cout << "F advantage" << endl;
+                else if ( scoreBoard[i].first == (scoreBoard[i].second + 1 ))
+                    cout << "D advantage" << endl;
+                else if ( scoreBoard[i].first == (scoreBoard[i].second + 2))
+                    cout << "D wins" << endl ;
+                else if ( (scoreBoard[i].first + 2) == scoreBoard[i].second )
+                    cout << "F wins" << endl ;
+            }
+            else if( scoreBoard[i].first > 3 || scoreBoard[i].second > 3 ) {
+                if( scoreBoard[i].first - scoreBoard[i].second >= 2 )
+                    cout << "D wins" << endl;
+                else if ( scoreBoard[i].second - scoreBoard[i].first >= 2 )
+                    cout << "F wins" << endl;
             }
             else
-                cout << scoreEquivalence[scoreBoard[i].first] << " " << scoreEquivalence[scoreBoard[i].second] << endl ;
+                cout << "D-" << scoreEquivalence[scoreBoard[i].first] << " F-" << scoreEquivalence[scoreBoard[i].second] << endl ;
         }
 };
 
@@ -97,6 +90,7 @@ int main() {
 	for(int i=0; i<playerScoreSequence.length(); i++) {
         tennisGame.updateScoreBoard(playerScoreSequence[i]);
 	}
-	tennisGame.displayWholeScoreboard(11);
+	tennisGame.displayFullScoreboard();
+	tennisGame.displayCurrentScore(5);
 	return 0;
 }
